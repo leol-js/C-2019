be612f338c513e1569da270d5382977269c70db6
leol <leol.programming@gmail.com>
2019-10-03 21:07:52 +0300

    fix

--- 9.c
+++ 9.c
@@ -2,10 +2,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
 #include <math.h>
-#include <omp.h>
-
+#include <time.h>
 
 #define TEST     100
 #define ELEMENTS 10000
@@ -34,19 +32,29 @@ struct statistic{
     double bestTime;
 };
 
-void testSort(int array[], int arraySize, int *times, int *ifs, int *swap, void(*sorter)()) {
+double timesSec() {
+    struct timespec tv;
+
+    clock_gettime(CLOCK_MONOTONIC, &tv);
+    return tv.tv_sec + ((double) tv.tv_nsec) / 1000000000;
+}
+
+double testSort(int array[], int arraySize, int *ifs, int *swap, void(*sorter)()) {
     int newArray[arraySize];
     cloneArray(array, newArray, arraySize);
 
-    *times = omp_get_wtime();
+    double a, b;
+    a = timesSec();
     sorter(newArray, arraySize, swap, ifs);
-    *times = omp_get_wtime() - *times;
+    b = timesSec();
+    return b - a;
 }
 
 int main() {
-    printf("--------------------------------------------------------------\n");
+    printf("-----------------------------------------------------------------\n");
     printf("%15s | %10s %10s %10s %10s\n", "Sort of:", "Insert", "Bubble", "Select", "Quick");
-    printf("--------------------------------------------------------------\n");
+    printf("-----------------------------------------------------------------\n");
+
 
     for (size_t elementsNumber = 10; elementsNumber < ELEMENTS; elementsNumber *= 10) {
         struct statistic insert = {0, 0, 0, 0, SIZE};
@@ -58,56 +66,57 @@ int main() {
         for (size_t elementsSize = 10; elementsSize < SIZE; elementsSize *= 10) {
             int array[elementsNumber];
             for (size_t i = 0; i < 100; i++) {
-                int swap, ifs, time;
+                int swap, ifs;
+                double times;
                 generateArray(array, elementsNumber, 0, elementsSize);
 
-                swap = 0, ifs = 0, time = 0;
-                testSort(array, elementsNumber, &time, &ifs, &swap, sortInsert);
-                insert.summaryTime += time;
+                swap = 0, ifs = 0, times = 0;
+                times = testSort(array, elementsNumber, &ifs, &swap, sortInsert);
+                insert.summaryTime += times;
                 insert.swap += swap;
                 insert.ifs += ifs;
-                if (insert.worseTime < time) insert.worseTime = time;
-                if (insert.bestTime > time) insert.bestTime = time;
+                if (insert.worseTime < times) insert.worseTime = times;
+                if (insert.bestTime > times) insert.bestTime = times;
 
-                swap = 0, ifs = 0, time = 0;
-                testSort(array, elementsNumber, &time, &ifs, &swap, sortBubble);
-                bubble.summaryTime += time;
+                swap = 0, ifs = 0, times = 0;
+                times = testSort(array, elementsNumber, &ifs, &swap, sortBubble);
+                bubble.summaryTime += times;
                 bubble.swap += swap;
                 bubble.ifs += ifs;
-                if (bubble.worseTime < time) bubble.worseTime = time;
-                if (bubble.bestTime > time) bubble.bestTime = time;
+                if (bubble.worseTime < times) bubble.worseTime = times;
+                if (bubble.bestTime > times) bubble.bestTime = times;
 
 
-                swap = 0, ifs = 0, time = 0;
-                testSort(array, elementsNumber, &time, &ifs, &swap, sortSelect);
-                select.summaryTime += time;
+                swap = 0, ifs = 0, times = 0;
+                times = testSort(array, elementsNumber, &ifs, &swap, sortSelect);
+                select.summaryTime += times;
                 select.swap += swap;
                 select.ifs += ifs;
-                if (select.worseTime < time) select.worseTime = time;
-                if (select.bestTime > time) select.bestTime = time;
+                if (select.worseTime < times) select.worseTime = times;
+                if (select.bestTime > times) select.bestTime = times;
 
 
-                swap = 0, ifs = 0, time = 0;
-                testSort(array, elementsNumber, &time, &ifs, &swap, sortQuick );
-                quick.summaryTime += time;
+                swap = 0, ifs = 0, times = 0;
+                times = testSort(array, elementsNumber, &ifs, &swap, sortQuick );
+                quick.summaryTime += times;
                 quick.swap += swap;
                 quick.ifs += ifs;
-                if (quick.worseTime < time) quick.worseTime = time;
-                if (quick.bestTime > time) quick.bestTime = time;
+                if (quick.worseTime < times) quick.worseTime = times;
+                if (quick.bestTime > times) quick.bestTime = times;
                 ops++;
             }
 
-            printf("%15s | %10d %10d %10d %10d\n", "Number of ops",   ops, ops, ops, ops);
-            printf("%15s | %10d %10d %10d %10d\n", "Number of elem",  elementsNumber, elementsNumber, elementsNumber, elementsNumber);
-            printf("%15s | %10d %10d %10d %10d\n", "Size of elem",    elementsSize, elementsSize, elementsSize, elementsSize);
-            printf("%15s | %10d %10d %10d %10d\n", "Number of ifs",   insert.ifs, bubble.ifs, select.ifs, quick.ifs);
-            printf("%15s | %10d %10d %10d %10d\n", "Number of swaps", insert.swap, bubble.swap, select.swap, quick.swap);
-            printf("%15s | %10lf %10lf %10lf %10lf\n", "Summary time",    insert.summaryTime, bubble.summaryTime, select.summaryTime, quick.summaryTime);
-            printf("%15s | %10lf %10lf %10lf %10lf\n", "Best time",       insert.bestTime, bubble.bestTime, select.bestTime, quick.bestTime);
-            printf("%15s | %10lf %10lf %10lf %10lf\n", "Worse time",      insert.worseTime, bubble.worseTime, select.worseTime, quick.worseTime);
-            printf("%15s | %10lf %10lf %10lf %10lf\n", "Middle time",     insert.summaryTime / ops, bubble.summaryTime / ops, select.summaryTime / ops, quick.summaryTime / ops);
-            printf("%15s | %10lf %10lf %10lf %10d\n", "Time / ElemNum",  insert.summaryTime / elementsNumber, bubble.summaryTime / elementsNumber, select.summaryTime / elementsNumber, quick.summaryTime / elementsNumber);
-            printf("--------------------------------------------------------------\n");
+            printf("%15s | %11d %11d %11d %11d\n", "Number of ops",   ops, ops, ops, ops);
+            printf("%15s | %11d %11d %11d %11d\n", "Number of elem",  elementsNumber, elementsNumber, elementsNumber, elementsNumber);
+            printf("%15s | %11d %11d %11d %11d\n", "Size of elem",    elementsSize, elementsSize, elementsSize, elementsSize);
+            printf("%15s | %11d %11d %11d %11d\n", "Number of ifs",   insert.ifs, bubble.ifs, select.ifs, quick.ifs);
+            printf("%15s | %11d %11d %11d %11d\n", "Number of swaps", insert.swap, bubble.swap, select.swap, quick.swap);
+            printf("%15s | %3.9lf %3.9lf %3.9lf %3.9lf\n", "Summary times",    insert.summaryTime, bubble.summaryTime, select.summaryTime, quick.summaryTime);
+            printf("%15s | %3.9lf %3.9lf %3.9lf %3.9lf\n", "Best times",       insert.bestTime, bubble.bestTime, select.bestTime, quick.bestTime);
+            printf("%15s | %3.9lf %3.9lf %3.9lf %3.9lf\n", "Worse times",      insert.worseTime, bubble.worseTime, select.worseTime, quick.worseTime);
+            printf("%15s | %3.9lf %3.9lf %3.9lf %3.9lf\n", "Middle times",     insert.summaryTime / ops, bubble.summaryTime / ops, select.summaryTime / ops, quick.summaryTime / ops);
+            printf("%15s | %3.9lf %3.9lf %3.9lf %3.9lf\n", "Time / ElemNum",  insert.summaryTime / elementsNumber, bubble.summaryTime / elementsNumber, select.summaryTime / elementsNumber, quick.summaryTime / elementsNumber);
+            printf("-----------------------------------------------------------------\n");
         }
     }
 
